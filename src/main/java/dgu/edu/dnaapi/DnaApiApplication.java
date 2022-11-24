5aff1469724918ee84fba456e0d76ace59ce5440
hojunlee-hj <cajun7@naver.com>
2022-11-24 11:20:14 +0900

    Initial setup

---
+++ src/main/java/dgu/edu/dnaapi/DnaApiApplication.java
@@ -0,0 +1,13 @@
+package dgu.edu.dnaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dgu/edu/dnaapi/DnaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dgu.edu.dnaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
