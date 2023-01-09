28368d2a1677c510591f2d2b9c8232d8a01b9293
Kyungmin Kim <qaws2051@gmail.com>
2023-01-10 01:56:39 +0900

    fix: 컨트롤러 리턴 타입 Long(id)에서 ResponseEntity로 수정

--- src/main/java/dgu/edu/dnaapi/controller/NoticesController.java
+++ src/main/java/dgu/edu/dnaapi/controller/NoticesController.java
@@ -4,9 +4,14 @@ import dgu.edu.dnaapi.domain.dto.NoticesResponseDto;
 import dgu.edu.dnaapi.domain.dto.NoticesSaveRequestDto;
 import dgu.edu.dnaapi.domain.dto.NoticesUpdateRequestDto;
 import dgu.edu.dnaapi.domain.response.Message;
+import dgu.edu.dnaapi.domain.response.ResponseEntity;
+import dgu.edu.dnaapi.domain.response.StatusEnum;
 import dgu.edu.dnaapi.service.NoticesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,22 +19,50 @@ public class NoticesController {
     private final NoticesService noticesService;
 
     @PostMapping("/boards/notices")
-    public Long save(@RequestBody NoticesSaveRequestDto requestDto) {
-        return noticesService.save(requestDto);
+    public ResponseEntity<Message> save(@RequestBody NoticesSaveRequestDto requestDto) {
+        Long savedId = noticesService.save(requestDto);
+        Message message = Message.builder()
+                .data(savedId)
+                .status(StatusEnum.OK)
+                .message(null)
+                .build();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        return new ResponseEntity(message, httpHeaders, HttpStatus.OK);
     }
 
     @PutMapping("/boards/notices")
-    public Long update(@RequestBody NoticesUpdateRequestDto requestDto) {
-        return noticesService.update(requestDto);
+    public ResponseEntity<Message> update(@RequestBody NoticesUpdateRequestDto requestDto) {
+        Long updateId = noticesService.update(requestDto);
+        Message message = Message.builder()
+                .data(updateId)
+                .status(StatusEnum.OK)
+                .message(null)
+                .build();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        return new ResponseEntity(message, httpHeaders, HttpStatus.OK);
     }
 
     @GetMapping("/boards/notices/{id}")
-    public NoticesResponseDto findById(@PathVariable Long id) {
-        return noticesService.findById(id);
+    public ResponseEntity<Message> findById(@PathVariable Long id) {
+        NoticesResponseDto responseDto = noticesService.findById(id);
+        Message message = Message.builder()
+                .data(responseDto)
+                .status(StatusEnum.OK)
+                .message(null)
+                .build();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        return new ResponseEntity(message, httpHeaders, HttpStatus.OK);
     }
 
     @DeleteMapping("/boards/notices")
-    public Long delete(@RequestBody NoticesUpdateRequestDto requestDto) {
-        return noticesService.delete(requestDto);
+    public ResponseEntity<Message> delete(@RequestBody NoticesUpdateRequestDto requestDto) {
+        Long deleteId = noticesService.delete(requestDto);
+        Message message = Message.builder()
+                .data(deleteId)
+                .status(StatusEnum.OK)
+                .message(null)
+                .build();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        return new ResponseEntity(message, httpHeaders, HttpStatus.OK);
     }
 }
