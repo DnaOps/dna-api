24860f3f8f5fc09fee40a36ad282fea53573d82b
Kyungmin Kim <qaws2051@gmail.com>
2023-01-12 17:43:57 +0900

    feat: 댓글 entity 추가, 공지사항의 author 필드 String -> User로 변경

--- src/main/java/dgu/edu/dnaapi/domain/dto/NoticesResponseDto.java
+++ src/main/java/dgu/edu/dnaapi/domain/dto/NoticesResponseDto.java
@@ -1,6 +1,7 @@
 package dgu.edu.dnaapi.domain.dto;
 
 import dgu.edu.dnaapi.domain.Notices;
+import dgu.edu.dnaapi.domain.User;
 import lombok.Getter;
 
 @Getter
@@ -8,7 +9,7 @@ public class NoticesResponseDto {
     private Long noticeId;
     private String title;
     private String content;
-    private String author;
+    private User author;
 
     public NoticesResponseDto(Notices entity) {
         this.noticeId = entity.getNoticeId();
